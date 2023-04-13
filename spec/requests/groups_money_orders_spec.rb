825ee519793dbbe45bd878c1f725444411e199ff
SimonGideon <simongideon918@gmail.com>
2023-04-13 22:54:33 +0300

    solve linters

--- spec/requests/groups_money_orders_spec.rb
+++ spec/requests/groups_money_orders_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "GroupsMoneyOrders", type: :request do
-  describe "GET /groups/:group_id/money_orders" do
-    let(:group) { Group.create(name: "Test Group") }
+RSpec.describe 'GroupsMoneyOrders', type: :request do
+  describe 'GET /groups/:group_id/money_orders' do
+    let(:group) { Group.create(name: 'Test Group') }
 
-    it "returns a successful response" do
+    it 'returns a successful response' do
       get group_money_orders_path(group)
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "POST /groups/:group_id/money_orders" do
-    let(:group) { Group.create(name: "Test Group") }
-    let(:params) { { money_order: { amount: 100, description: "Test Money Order" } } }
+  describe 'POST /groups/:group_id/money_orders' do
+    let(:group) { Group.create(name: 'Test Group') }
+    let(:params) { { money_order: { amount: 100, description: 'Test Money Order' } } }
 
-    it "creates a new money order" do
-      expect {
-        post group_money_orders_path(group), params: params
-      }.to change(MoneyOrder, :count).by(1)
+    it 'creates a new money order' do
+      expect do
+        post group_money_orders_path(group), params:
+      end.to change(MoneyOrder, :count).by(1)
     end
   end
 end

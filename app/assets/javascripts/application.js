412c9e10879ce6442f7639da0feb09c84174b994
SimonGideon <simongideon918@gmail.com>
2023-07-15 12:27:52 +0300

    notice

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,11 @@
-//= require jquery 
-//= require jquery_ujs
+document.addEventListener('DOMContentLoaded', function() {
+    const contElement = document.querySelector('.cont');
+    const noticeElement = document.querySelector('.notice');
+    
+    if (contElement.textContent.trim().length === 0) {
+      noticeElement.style.display = 'none';
+    } else {
+      noticeElement.style.display = 'block';
+    }
+  });
+  

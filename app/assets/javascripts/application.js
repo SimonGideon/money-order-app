80d014be9c618c3254d48c2e172a189e261a34e6
SimonGideon <simongideon918@gmail.com>
2023-07-15 12:39:54 +0300

    feat: nofocation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,2 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const contElement = document.querySelector('.cont');
-    const noticeElement = document.querySelector('.notice');
-    
-    if (contElement.textContent.trim().length === 0) {
-      noticeElement.style.display = 'none';
-    } else {
-      noticeElement.style.display = 'block';
-    }
-  });
+
   
